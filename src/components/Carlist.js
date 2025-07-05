8fafa4fabb4ab1fdb35134adfddd2e920e632c05
Car_Bit <olegkorbovskij@gmail.com>
2025-07-06 01:53:01 +0300

    новые изменения всоответствии с версией MUI 6.0

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+/*import React, { useEffect, useState } from "react";
 import { SERVER_URL } from "../constants.js";
 import { DataGrid } from "@mui/x-data-grid";
 import Snackbar from "@mui/material/Snackbar";
@@ -111,4 +111,137 @@ function Carlist() {
     </React.Fragment>
   );
 }
+export default Carlist;
+*/
+import React, { useEffect, useState } from "react";
+import { SERVER_URL } from "../constants.js";
+import { DataGrid } from "@mui/x-data-grid";
+import Snackbar from "@mui/material/Snackbar";
+import AddCar from "./AddCar.js";
+import EditCar from "./EditCar.js";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+function Carlist() {
+  const [cars, setCars] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const columns = [
+    { field: "brand", headerName: "Brand", width: 200 },
+    { field: "model", headerName: "Model", width: 200 },
+    { field: "color", headerName: "Color", width: 200 },
+    { field: "yearOfCar", headerName: "Year", width: 150 },
+    { field: "price", headerName: "Price", width: 150 },
+    {
+      field: "_links.car.href",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      renderCell: (row) => <EditCar data={row} updateCar={updateCar} />,
+    },
+    {
+      field: "_links.self.href",
+      headerName: "",
+      sortable: false,
+      filterable: false,
+      renderCell: (row) => (
+        <Button onClick={() => onDelClick(row.id)} color="error">
+          Delete
+        </Button>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    fetchCars();
+  }, []);
+
+  const fetchCars = () => {
+    fetch(SERVER_URL + "api/cars")
+      .then((response) => response.json())
+      .then((data) => setCars(data._embedded.cars))
+      .catch((err) => console.error(err));
+  };
+
+  const onDelClick = (url) => {
+    if (window.confirm("Are you sure to delete?")) {
+      fetch(url, { method: "DELETE" })
+        .then((response) => {
+          if (response.ok) {
+            fetchCars();
+            setOpen(true);
+          } else {
+            alert("Something went wrong!");
+          }
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+
+  const addCar = (car) => {
+    fetch(SERVER_URL + "api/cars", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car),
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchCars();
+        } else {
+          alert("Something went wrong!");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const updateCar = (car, link) => {
+    fetch(link, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(car),
+    })
+      .then((response) => {
+        if (response.ok) {
+          fetchCars();
+        } else {
+          alert("Something went wrong!");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <React.Fragment>
+      <Box display="flex" justifyContent="center" alignItems="center" mb={2}>
+        <AddCar addCar={addCar} />
+      </Box>
+      <div style={{ height: 500, width: "100%" }}>
+        <DataGrid
+          rows={cars}
+          columns={columns}
+          disableRowSelectionOnClick={true}
+          getRowId={(row) => row._links.self.href}
+          showToolbar
+          slotProps={{
+            toolbar: {
+              csvOptions: {
+                fileName: "cars_list",
+                delimiter: ",",
+                utf8WithBom: true,
+              },
+              printOptions: { disableToolbarButton: true },
+            },
+          }}
+        />
+        <Snackbar
+          open={open}
+          autoHideDuration={2000}
+          onClose={() => setOpen(false)}
+          message="Car deleted"
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+
 export default Carlist;
