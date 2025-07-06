3dae071cbf9ebb73b7643f127aa82fe9af259e57
Car_Bit <olegkorbovskij@gmail.com>
2025-07-07 00:24:44 +0300

    событийный тест для модального окна "New Car"

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,20 @@
-import { render, screen } from '@testing-library/react';
+/*import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/carshop/i);
+  expect(linkElement).toBeInTheDocument();
+});
+*/
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+test("open add car modal form", () => {
+  render(<App />);
+  fireEvent.click(screen.getByText("New Car"));
+  expect(screen.getByRole("dialog")).toHaveTextContent("New car");
+});
 
 test('renders learn react link', () => {
   render(<App />);
