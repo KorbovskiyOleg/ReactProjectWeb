50185a4b0015bd1e11c6c0212d76ccf4a525e559
Car_Bit <olegkorbovskij@gmail.com>
2025-07-07 03:28:45 +0300

    добавил базовый код

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { SERVER_URL } from "../constants.js";
 
-
-
 function Login() {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const [isAuthenticated, setAuth] = useState(false);
+
   return <div></div>;
 }
 export default Login;
