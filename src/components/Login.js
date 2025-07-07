f27ac4585e15ecf7e2e4e8fe01b1360b64cf3192
Car_Bit <olegkorbovskij@gmail.com>
2025-07-07 03:41:18 +0300

    реализовал оператор возврата

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { SERVER_URL } from "../constants.js";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+import Carlist from "./Carlist";
 
 function Login() {
   const [user, setUser] = useState({
@@ -11,44 +12,45 @@ function Login() {
   });
   const [isAuthenticated, setAuth] = useState(false);
 
-   const handleChange = (event) => {
-  setUser({...user, 
-     [event.target.name] : event.target.value});
- }
-
+  const handleChange = (event) => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
 
   const login = () => {
-    fetch(SERVER_URL + 'login', {
-      method: 'POST',
-      headers: { 'Content-Type':'application/json' },
-      body: JSON.stringify(user)
+    fetch(SERVER_URL + "login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
     })
-    .then(res => {
-      const jwtToken = res.headers.get
-          ('Authorization');
-      if (jwtToken !== null) {
-        sessionStorage.setItem("jwt", jwtToken);
-        setAuth(true);
-      }
-    })
-    .catch(err => console.error(err))
- }
+      .then((res) => {
+        const jwtToken = res.headers.get("Authorization");
+        if (jwtToken !== null) {
+          sessionStorage.setItem("jwt", jwtToken);
+          setAuth(true);
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
-  return (
-    <div>
-      <Stack spacing={2} alignItems="center" mt={2}>
-        <TextField name="username" label="Username" onChange={handleChange} />
-        <TextField
-          type="password"
-          name="password"
-          label="Password"
-          onChange={handleChange}
-        />
-        <Button variant="outlined" color="primary" onClick={login}>
-          Login
-        </Button>
-      </Stack>
-    </div>
-  );
+  if (isAuthenticated) {
+    return <Carlist />;
+  } else {
+    return (
+      <div>
+        <Stack spacing={2} alignItems="center" mt={2}>
+          <TextField name="username" label="Username" onChange={handleChange} />
+          <TextField
+            type="password"
+            name="password"
+            label="Password"
+            onChange={handleChange}
+          />
+          <Button variant="outlined" color="primary" onClick={login}>
+            Login
+          </Button>
+        </Stack>
+      </div>
+    );
+  }
 }
 export default Login;
