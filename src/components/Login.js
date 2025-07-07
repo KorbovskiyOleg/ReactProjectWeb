79bd220e6c7b1e17887cf0f725b7cf9ad1e5d9e0
Car_Bit <olegkorbovskij@gmail.com>
2025-07-07 03:38:15 +0300

    реализовал функцию вызова

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,6 +16,24 @@ function Login() {
      [event.target.name] : event.target.value});
  }
 
+
+  const login = () => {
+    fetch(SERVER_URL + 'login', {
+      method: 'POST',
+      headers: { 'Content-Type':'application/json' },
+      body: JSON.stringify(user)
+    })
+    .then(res => {
+      const jwtToken = res.headers.get
+          ('Authorization');
+      if (jwtToken !== null) {
+        sessionStorage.setItem("jwt", jwtToken);
+        setAuth(true);
+      }
+    })
+    .catch(err => console.error(err))
+ }
+
   return (
     <div>
       <Stack spacing={2} alignItems="center" mt={2}>
