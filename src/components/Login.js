a5755815ffd5ce3f1f264f25030dde01a1fe8b26
Car_Bit <olegkorbovskij@gmail.com>
2025-07-07 03:34:07 +0300

    добавил компоненты, реализовал функцию обрабртки изменений

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { SERVER_URL } from "../constants.js";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 function Login() {
   const [user, setUser] = useState({
@@ -8,6 +11,26 @@ function Login() {
   });
   const [isAuthenticated, setAuth] = useState(false);
 
-  return <div></div>;
+   const handleChange = (event) => {
+  setUser({...user, 
+     [event.target.name] : event.target.value});
+ }
+
+  return (
+    <div>
+      <Stack spacing={2} alignItems="center" mt={2}>
+        <TextField name="username" label="Username" onChange={handleChange} />
+        <TextField
+          type="password"
+          name="password"
+          label="Password"
+          onChange={handleChange}
+        />
+        <Button variant="outlined" color="primary" onClick={login}>
+          Login
+        </Button>
+      </Stack>
+    </div>
+  );
 }
 export default Login;
